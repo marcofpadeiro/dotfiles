8a881b0a43ba49c4a2d98c8496c315628451e25a
Marco Padeiro <marcofpadeiro@gmail.com>
2025-05-15 19:48:27 +0100

    Make bar invisible with keybind and adding power usage on bar

--- sway/.config/sway/bar.sh
+++ sway/.config/sway/bar.sh
@@ -1,5 +1,6 @@
 charging_status=$(cat /sys/class/power_supply/BAT0/status)
 battery_percentage=$(cat /sys/class/power_supply/BAT0/capacity)
+power_draw=$(echo "scale=2; $(cat /sys/class/power_supply/BAT0/power_now) / 1000000" | bc)
 date=$(date +'%Y-%m-%d %X')
 lang=$(swaymsg -t get_inputs | jq 'map(select(has("xkb_active_layout_name")))[1].xkb_active_layout_name')
 
@@ -14,4 +15,4 @@ if [ "$charging_status" = "Charging" ]; then
     charging_icon=""
 fi
 
-echo "$charging_icon $battery_percentage% | $lang | $date"
+echo "$charging_icon $battery_percentage% (${power_draw}W) | $lang | $date"
