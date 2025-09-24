e8a6ed44c478dcfc47367ad9daf626701789089b
Marco Padeiro <marcofpadeiro@proton.me>
2025-09-24 12:20:00 +0100

    Adding initial structure for a DAP

---
+++ nvim/.config/nvim/lua/plugins/dap.lua
@@ -0,0 +1,37 @@
+local ok_d, dap = pcall(require, 'dap')
+if not ok_d then return end
+local ok_ui, dap_ui = pcall(require, 'dapui')
+if not ok_ui then return end
+local ok_vt, dap_virtual_text = pcall(require, 'nvim-dap-virtual-text')
+if not ok_vt then return end
+
+dap_ui.setup()
+dap_virtual_text.setup()
+
+vim.keymap.set("n", "<space>b", dap.toggle_breakpoint)
+vim.keymap.set("n", "<space>gb", dap.run_to_cursor)
+
+-- Eval var under cursor
+vim.keymap.set("n", "<space>?", function()
+    require("dapui").eval(nil, { enter = true })
+end)
+
+vim.keymap.set("n", "<F1>", dap.continue)
+vim.keymap.set("n", "<F2>", dap.step_into)
+vim.keymap.set("n", "<F3>", dap.step_over)
+vim.keymap.set("n", "<F4>", dap.step_out)
+vim.keymap.set("n", "<F5>", dap.step_back)
+vim.keymap.set("n", "<F13>", dap.restart)
+
+dap.listeners.before.attach.dapui_config = function()
+    ui.open()
+end
+dap.listeners.before.launch.dapui_config = function()
+    ui.open()
+end
+dap.listeners.before.event_terminated.dapui_config = function()
+    ui.close()
+end
+dap.listeners.before.event_exited.dapui_config = function()
+    ui.close()
+end
