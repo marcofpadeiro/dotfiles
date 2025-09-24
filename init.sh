cda949fb5f92d5be4385394eebfa78267a0b1f4f
Marco Padeiro <marcofpadeiro@proton.me>
2025-09-24 22:50:09 +0100

    Changing so no longer stow needed Adding init script

---
+++ init.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+ln -s $PWD/nvim $HOME/.config/nvim
+ln -s $PWD/zsh $HOME/.config/zsh
+ln -s $PWD/tmux $HOME/.config/tmux
+ln -s $PWD/zathura $HOME/.config/zathura
+sudo ln -s $PWD/tlp/tlp.conf /etc/tlp.conf
+sudo ln -s $PWD/throttled/throttled.conf /etc/throttled.conf
+
--- scripts/fzf-tmux.sh
+++ scripts/fzf-tmux.sh
@@ -1,18 +1,37 @@
 #!/bin/bash
 
-session=$(find ~/personal ~/uni ~/dotfiles -maxdepth 2 -type d | fzf)
-session_name=$(basename "$session" | tr . _) 
+use_find() {
+    local args=("$@")
+    local depth="${args[-1]}"
+    unset "args[${#args[@]}-1]"
 
-if ! tmux has-session -t "$session_name" 2> /dev/null; then
-    tmux new-session -d -s "$session_name" -c "$session"
-    tmux send-keys -t "$session_name" "nvim" Enter
-    tmux new-window -t "$session_name" -c "$session" -d
+    for root in "${args[@]}"; do
+        find "$root" -mindepth 1 -maxdepth "$depth" \
+            \( -type d -name '.*' -prune \) -o -type d -print
+    done
+}
+
+# usage: directories first, then depth
+depth1=$(use_find "$HOME/personal" "$HOME/dotfiles" 1)
+depth2=$(use_find "$HOME/uni" 2)
+echo "$depth1"
+echo "$depth2"
+
+session=$(echo -e "$depth1\n$depth2\n$HOME/dotfiles" | fzf)
+session_name=$(basename "$session" | tr . _)
+
+if ! tmux has-session -t "$session_name" 2>/dev/null; then
+    tmux new-session -d -s "$session_name" -c "$session" -n nvim 'nvim'
+
+    tmux new-window -t "$session_name":2 -c "$session" -n zsh 'zsh'
+
+    tmux new-window -t "$session_name":3 -c "$session" -n git 'lazygit'
+
+    tmux select-window -t "$session_name":1
 fi
 
-if [[ "${TMUX-}" != '' ]];
-then
+if [ -n "${TMUX-}" ]; then
     tmux switch-client -t "$session_name"
+else
+    tmux attach-session -t "$session_name"
 fi
-
-tmux attach-session -t "$session_name"
-
